e2be0e07de2af2746abd221897d22ab51309b651
simone <simone44c@gmail.com>
2023-06-15 00:47:09 +0200

    commit 1.5.7

--- script/index.js
+++ script/index.js
@@ -1,45 +1,76 @@
 
 
 
-
+// creazione tabellone
 let numberToAdd=function(number){
     let numberDiv=document.getElementById('main-content')
-    for(let i=0;i<91;i++){
+    for(let i=1;i<91;i++){
         let tabNumber=document.createElement('div')
         tabNumber.classList.add('tab-number')
         let numberTombola=document.createElement('p')
-        numberTombola.classList.add('cellah3')
+        numberTombola.classList.add('cellap')
         numberTombola.innerText=[i]
-         
         
-      tabNumber.appendChild(numberTombola)
+      
         numberDiv.appendChild(tabNumber)
+        tabNumber.appendChild(numberTombola)
+    }
+    }
+      numberToAdd()
 
-    }
-    }
+// generatore numeri casuale
+    const myArray=[]
+    
+        let myButton=document.getElementById('button1')
+        myButton.addEventListener('click',function(){
+        let numbers=Math.floor(Math.random() *90 + 1)
+        console.log(numbers)
+        
+        // colorazione numeri estratti
+        let myD=document.querySelectorAll('.tab-number')
+        myD.forEach((n)=>{
+            if(parseInt(n.innerText)===numbers){
+                n.classList.add('firstclass')
+                
+            }
+        })
+    })
 
    
-    numberToAdd()
+
+           
+        
+    
 
     
-        
-            let button=document.getElementById('button')
-            button.addEventListener('click',function(e){
-                let casualNumbers=Math.floor(Math.random()*91)
-                document.querySelector('.nuovo').innerText=casualNumbers
-                
-                
-                
-                let mydivcontainer=document.querySelectorAll('.tab-number')
-                mydivcontainer.forEach((element)=>{
-                    if(Number(element.innerText===casualNumbers)){
-                        element.classList.add('color')
-                    }
-                })
-            })
- 
-            
+
     
+
+       
+
+ 
+
+
+        
+    
+    
+    
+
+    
+
+
+    
+
+        
+
+    
+
+
+
+
+    
+ 
+ 
         
                 
 
